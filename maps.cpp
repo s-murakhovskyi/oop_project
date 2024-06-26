f0f242939de967d52c2c29057aaf8b37e190ef0c
s-murakhovskyi <smurakhovskyi2004@gmail.com>
2024-06-27 00:43:50 +0200

    parent class Map, class shop

--- maps.cpp
+++ maps.cpp
@@ -1,8 +1,53 @@
-#include "maps.h"
 #include <iostream>
+#include "maps.h"
+
 using namespace std;
-int main()
-{
-	cout << "test";
-	cout << "should work";
+
+void displayMenu() {
+    cout << "Map Shop" << endl;
+    cout << "1. Add Map" << endl;
+    cout << "2. Display All Maps" << endl;
+    cout << "3. Modify Map Data" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Choose an option: ";
+}
+
+int main() {
+    Shop shop;
+    int choice;
+
+    while (true) {
+        displayMenu();
+
+        cin >> choice;
+        if (choice == 4) break;
+
+        string name;
+        double scale;
+        switch (choice) {
+        case 1: {
+            cout << "Enter map name: ";
+            cin >> name;
+            cout << "Enter scale: ";
+            cin >> scale;
+            shop.addMap(new Map(name, scale));
+            break;
+        }
+        case 2: {
+            shop.displayAllMaps();
+            break;
+        }
+        case 3: {
+            cout << "Enter the name of the map to modify: ";
+            cin >> name;
+            shop.modifyMapData(name);
+            break;
+        }
+        default: {
+            cout << "Invalid option. Try again." << endl;
+            break;
+        }
+        }
+    }
+    return 0;
 }
--- maps.h
+++ maps.h
@@ -1,5 +1,61 @@
-#pragma once
-class maps
-{
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Map {
+protected:
+    string name;
+    double scale;
+
+public:
+    Map(const string& name, double scale) : name(name), scale(scale) {}
+
+    virtual ~Map() = default;
+
+    string getName() const { return name; }
+    double getScale() const { return scale; }
+    void setScale(double newScale) { scale = newScale; }
+
+    virtual void displayInfo() const {
+        cout << "Map: " << name << ", Scale: 1:" << scale << endl;
+    }
+
+    virtual void modifyData() {
+        cout << "Enter new scale: ";
+        cin >> scale;
+    }
 };
 
+class Shop {
+private:
+    vector<Map*> maps;
+
+public:
+    ~Shop() {
+        for (auto map : maps) {
+            delete map;
+        }
+    }
+
+    void addMap(Map* map) {
+        maps.push_back(map);
+    }
+
+    void displayAllMaps() const {
+        for (const auto& map : maps) {
+            map->displayInfo();
+        }
+    }
+
+    void modifyMapData(const string& mapName) {
+        for (auto& map : maps) {
+            if (map->getName() == mapName) {
+                map->modifyData();
+                return;
+            }
+        }
+        cout << "Map not found." << endl;
+    }
+};
