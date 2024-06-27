854aba4768abcc7bb39d4cc51e1bb157d995af98
s-murakhovskyi <smurakhovskyi2004@gmail.com>
2024-06-27 18:46:19 +0200

    added types of maps

--- maps.cpp
+++ maps.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 void displayMenu() {
-    cout << "Map Shop" << endl;
-    cout << "1. Add Map" << endl;
-    cout << "2. Display All Maps" << endl;
-    cout << "3. Modify Map Data" << endl;
-    cout << "4. Exit" << endl;
+    cout << "Hi, you are in Map Shop! Create your maps and take them whenever you are ready!" << endl;
+    cout << "1. Add Political Map" << endl;
+    cout << "2. Add Physical Map" << endl;
+    cout << "3. Add Geological Map" << endl;
+    cout << "4. Display All Maps" << endl;
+    cout << "5. Modify Map Data" << endl;
+    cout << "6. Exit" << endl;
     cout << "Choose an option: ";
 }
 
@@ -18,29 +20,52 @@ int main() {
 
     while (true) {
         displayMenu();
-
         cin >> choice;
-        if (choice == 4) break;
-
+        if (choice == 6) break;
         string name;
         double scale;
         switch (choice) {
         case 1: {
+            string region;
             cout << "Enter map name: ";
             cin >> name;
             cout << "Enter scale: ";
             cin >> scale;
-            shop.addMap(new Map(name, scale));
+            cout << "Enter region: ";
+            cin >> region;
+            shop.addMap(new PoliticalMap(name, scale, region));
             break;
         }
         case 2: {
-            shop.displayAllMaps();
+            string terrainType;
+            cout << "Enter map name: ";
+            cin >> name;
+            cout << "Enter scale: ";
+            cin >> scale;
+            cout << "Enter terrain type: ";
+            cin >> terrainType;
+            shop.addMap(new PhysicalMap(name, scale, terrainType));
             break;
         }
         case 3: {
+            string geologyType;
+            cout << "Enter map name: ";
+            cin >> name;
+            cout << "Enter scale: ";
+            cin >> scale;
+            cout << "Enter geology type: ";
+            cin >> geologyType;
+            shop.addMap(new GeologicalMap(name, scale, geologyType));
+            break;
+        }
+        case 4: {
+            shop.displayAllMaps();
+            break;
+        }
+        case 5: {
             cout << "Enter the name of the map to modify: ";
             cin >> name;
-            shop.modifyMapData(name);
+            shop.modifyMapData(name, scale);
             break;
         }
         default: {
--- maps.h
+++ maps.h
@@ -18,11 +18,68 @@ public:
     double getScale() const { return scale; }
     void setScale(double newScale) { scale = newScale; }
 
-    virtual void displayInfo() const {
-        cout << "Map: " << name << ", Scale: 1:" << scale << endl;
+    virtual void displayInfo() const = 0;
+    virtual void modifyData() = 0;
+};
+
+class PoliticalMap : public Map {
+private:
+    string region;
+
+public:
+    PoliticalMap(const string& name, double scale, const string& region)
+        : Map(name, scale), region(region) {}
+
+    void displayInfo() const override {
+        cout << "Political Map: " << name << ", Scale: 1:" << scale
+            << ", Region: " << region << endl;
     }
 
-    virtual void modifyData() {
+    void modifyData() override {
+        cout << "Enter new region: ";
+        cin >> region;
+        cout << "Enter new scale: ";
+        cin >> scale;
+    }
+};
+
+class PhysicalMap : public Map {
+private:
+    string terrainType;
+
+public:
+    PhysicalMap(const string& name, double scale, const string& terrainType)
+        : Map(name, scale), terrainType(terrainType) {}
+
+    void displayInfo() const override {
+        cout << "Physical Map: " << name << ", Scale: 1:" << scale
+            << ", Terrain Type: " << terrainType << endl;
+    }
+
+    void modifyData() override {
+        cout << "Enter new terrain type: ";
+        cin >> terrainType;
+        cout << "Enter new scale: ";
+        cin >> scale;
+    }
+};
+
+class GeologicalMap : public Map {
+private:
+    string geologyType;
+
+public:
+    GeologicalMap(const string& name, double scale, const string& geologyType)
+        : Map(name, scale), geologyType(geologyType) {}
+
+    void displayInfo() const override {
+        cout << "Geological Map: " << name << ", Scale: 1:" << scale
+            << ", Geology Type: " << geologyType << endl;
+    }
+
+    void modifyData() override {
+        cout << "Enter new geology type: ";
+        cin >> geologyType;
         cout << "Enter new scale: ";
         cin >> scale;
     }
@@ -49,7 +106,7 @@ public:
         }
     }
 
-    void modifyMapData(const string& mapName) {
+    void modifyMapData(const string& mapName, const double& mapScale) {
         for (auto& map : maps) {
             if (map->getName() == mapName) {
                 map->modifyData();
